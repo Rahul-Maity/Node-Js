9665b2fda0ce1ec9717c2b169e981160e34905d7
Rahul <mrahulmaity623@gmail.com>
2023-10-07 16:44:45 +0530

    app object in Express Js

--- Express Js/index.js
+++ Express Js/index.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const app = express();
-// app.use(express.static(`./public/`, {
-//     index:'home.html',
-// }));
+/*
+app.locals.title = 'My App';
+app.get('/', (req, res) => {
+    console.log(app.locals.title);
+    res.send('This is home page now');
+})
 
-const router = express.Router({
-    caseSensitive:true,
+const admin = express();
+app.get('/', (req, res) => {
+    res.send('Welcome to application home');
+})
+admin.on('mount', (parent) => {
+    console.log(parent);
+})
+admin.get('/dashboard', (req, res) => {
+    res.send('Welcome to admin dashboard');
+    console.log(admin.mountpath);
 });
-router.get('/', (req, res) => {
-    res.send('This is home page');
+app.use('/admin', admin);
+app.enable('case sensitive routing');
+app.disable('case sensitive routing');
+app.all('/about', (req, res) => {
+    res.send('Welcome to application home');
 })
-router.post('/', (req, res) => {
-    res.send('home page with post request');
-    // console.log( req.body.toString());//<-parsing raw data octate string
-    console.log(req.body);
+app.param('id', (req, res, next, id) => {
+    const user = {
+        userId: id,
+        name: 'India'
+    };
+    req.userDetails = user;
+    next();
+});
+app.get('/user/:id', (req, res) => {
+    console.log(req.userDetails);
+    res.send('Welcome to app home');
 })
+*/
+app.set('view engine', 'ejs');
+app.route('/about/mission')
+    .get((req, res)=> {
+        res.render('pages/about');
+    })
+    .put((req, res)=> {
+        res.send('Welcome to put');
+    })
+    .post((req, res)=> {
+        res.send('Welcome to post ');
+    })
 app.listen(3000, () => {
     console.log('listning on port 3000');
-})
+});
