314951e66e576a76417aca3cf143501e5ffd525a
Rahul <mrahulmaity623@gmail.com>
2023-10-07 11:41:32 +0530

    express parsing function

--- Express Js/index.js
+++ Express Js/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+// app.use(express.json());
+// app.use(express.raw());
+// app.use(express.text());
+app.use(express.urlencoded());
 app.get('/', (req, res) => {
     res.send('This is home page');
 })
 app.post('/', (req, res) => {
     res.send('home page with post request');
+    // console.log( req.body.toString());//<-parsing raw data octate string
+    console.log(req.body);
 })
 app.listen(3000, () => {
     console.log('listning on port 3000');
